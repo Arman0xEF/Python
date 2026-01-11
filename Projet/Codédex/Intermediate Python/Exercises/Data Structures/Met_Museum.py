877fefb63bd57e1e03c4c44f99f23143118fb72d
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 21:01:47 +0100

    Add Met Museum data structure example

---
+++ Projet/Codédex/Intermediate Python/Exercises/Data Structures/Met_Museum.py
@@ -0,0 +1,9 @@
+Rosewater_ewer = {
+    'artist': 'Johann Erhard Heiglen',
+    'date': 1780,
+    'culture': 'German'
+}
+
+print("Dictionary :", Rosewater_ewer)
+print("Keys :", Rosewater_ewer.keys())
+print("Values :", Rosewater_ewer.values())
