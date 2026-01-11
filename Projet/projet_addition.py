d526964c44a913037519c0f086c41b7102a7dbce
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 15:56:42 +0100

    Add basic addition program in projet_addition.py

---
+++ Projet/projet_addition.py
@@ -0,0 +1,8 @@
+a=b=""
+while not (a.isdigit() and b.isdigit()):
+    a = input("Veuillez entrer un premier nombre :")
+    b = input("Veuillez entrer un deuxieme nombre :")
+    if not (a.isdigit() and b.isdigit()):
+        print("Veuillez entrer deux nombres valides")
+
+print(f"Le résultat de l'addition de {a} avec {b} est égal à {int(a) + int(b)}")
