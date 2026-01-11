b800d1a84046696047cb282acf34694ae91124c5
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 16:26:49 +0100

    Add basic calculator functions in Calculator.py

---
+++ Projet/Codédex/Python Beginner/Exercises/Functions/Calculator.py
@@ -0,0 +1,20 @@
+def add(a, b):
+  return a + b
+
+def substract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
+
+def exp(a, b):
+  return a ** b
+
+print(add(5 ,5))
+print(substract(5 ,5))
+print(multiply(5 ,5))
+print(divide(5 ,5))
+print(exp(5 ,5))
