a2875153c080110c958c77273d2939ae7cf9f793
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 21:01:37 +0100

    Add Find_My_Friends.py to display friend locations

---
+++ Projet/Codédex/Intermediate Python/Exercises/Data Structures/Find_My_Friends.py
@@ -0,0 +1,10 @@
+my_city = ('France', 48.657407, 2.754694)
+friend1 = (39.735706, -4.459216)
+friend2 = (50.981776, 9.427012)
+friend3 = (64.939252, 19.361811)
+
+print(f"My friend who is the furthest away from me ({my_city[0]}) is : {friend3}")
+
+all_friends = friend1 + friend2 + friend3
+
+print(f"Here is all my friends location : {all_friends}")
