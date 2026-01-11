b61e85402904cbca8e6032796b15b0ddf05bdf6a
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 21:02:00 +0100

    Add fruit cart functionality with union and intersection

---
+++ Projet/Codédex/Intermediate Python/Exercises/Data Structures/Fruit_Cart.py
@@ -0,0 +1,8 @@
+my_favorite_fruits = {'🍌 Banana','🍓 Strawberry','🍒 Cherry'}
+friend_favorite_fruits = {'🫐 Blueberry', '🍇 Blackberry', '🍌 Banana'}
+
+union = my_favorite_fruits.union(friend_favorite_fruits)
+inter = my_favorite_fruits.intersection(friend_favorite_fruits)
+
+print(union)
+print(inter)
