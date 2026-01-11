fe78732c1ad4c4c4f9f3bd2110ae51abe61c72ec
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 16:09:22 +0100

    Add Magicball.py for fortune-telling game

---
+++ Projet/Codédex/Python Beginner/Exercises/Control Flow/Magicball.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(1, 9)
+question = input("Question : ")
+
+if number == 1:
+  print("Yes - definitely.")
+elif number == 2:
+  print("It is decidedly so.")
+elif number == 3:
+  print("Without a doubt.")
+elif number == 4:
+  print("Reply hazy, try again.")
+elif number == 5:
+  print("Ask again later.")
+elif number == 6:
+  print("Better not tell you now.")
+elif number == 7:
+  print("My sources say no.")
+elif number == 8:
+  print("Outlook not so good.")
+else:
+  print("Very doubtful.")
