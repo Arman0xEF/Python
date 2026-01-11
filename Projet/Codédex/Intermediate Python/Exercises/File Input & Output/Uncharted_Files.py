222814a0386314f0d61e1720620979ccf6743ba7
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 21:03:27 +0100

    Add file input/output example for message handling

---
+++ Projet/Codédex/Intermediate Python/Exercises/File Input & Output/Uncharted_Files.py
@@ -0,0 +1,14 @@
+sent_message = 'Hey there! This is a secret message.'
+
+with open('sent_message.txt', 'w') as file:
+  file.write(sent_message)
+
+with open('sent_message.txt', 'r+') as file:
+  original_message = file.read()
+  file.seek(0)
+  unsent_message = 'This message has been unsent.'
+  file.truncate(len(unsent_message))
+  file.write(unsent_message)
+
+print("Original message :",original_message)
+print("Unsent message : ",unsent_message)
