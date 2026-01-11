5d9e10c3b0f7da5cc6f79e863a67f71ebd0c6490
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 16:28:21 +0100

    Add dog_years function to calculate human age

---
+++ Projet/Codédex/Python Beginner/Exercises/Functions/Dog_Years.py
@@ -0,0 +1,7 @@
+def dog_years(name, age):
+  x = age * 7
+  return f"{name} is {x} years old in human years."
+
+print(dog_years('Landon', 3))
+print(dog_years('Red Bean', 6))
+print(dog_years('Cooper', 2))
