74a94ff738a4b1cb6c3b0fee3906f6175e360a63
Arman Abgaryan <arman.abgaryan@etudiants.ecp-apprentissage.fr>
2026-01-11 21:03:07 +0100

    Add Dear_Diary.py to write diary entries

---
+++ Projet/Codédex/Intermediate Python/Exercises/File Input & Output/Dear_Diary.py
@@ -0,0 +1,11 @@
+file = open('diaries.txt', 'w')
+file.write("Dear diary,\nIn March, me and Mole decided to go out swimming. We both arrived together. While Mole was getting dry. I was \
+swimming. Minutes later, me and mole got to the stage that we did not know what to do so we checked on Toad. He was alright so we all decided to have \
+a picnic in the wild woods.\nOne hour later, we had finished our yummy picnic. We \
+wondered if Badger was doing OK but when we got to\
+Badger’s underground house the weasels suddenly turned\
+up.\nIt was late at night. When the weasels saw us it scared us,\
+especially when they went “Raw” After the weasels left we\
+went to see Badger. He wasn’t very happy that the weasels\
+were mucking about. After we left Badger we saw a cart\
+that Toad had bought so we drove onto the open road. ")
